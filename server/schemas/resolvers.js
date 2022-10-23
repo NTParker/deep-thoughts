f4a90d25825b8fb57d26c8b3c5bb612323f46f7e
Noah Parker <noahtmparker@gmail.com>
2022-10-23 19:51:23 -0400

    finished client side query for homepage/added full-stack structure

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,30 +1,9 @@
-const { User, Thought } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { User, Thought } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    thoughts: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Thought.find(params).sort({ createdAt: -1 });
-    },
-    thoughts: async (parent, { _id }) => {
-      return Thought.findOne({ _id });
-    },
-    // get all users
-    users: async () => {
-      return User.find()
-        .select('-__v -password')
-        .populate('friends')
-        .populate('thoughts');
-    },
-    // get a user by username
-    user: async (parent, { username }) => {
-      return User.findOne({ username })
-        .select('-__v -password')
-        .populate('friends')
-        .populate('thoughts');
-    },
     me: async (parent, args, context) => {
       if (context.user) {
         const userData = await User.findOne({ _id: context.user._id })
@@ -37,6 +16,25 @@ const resolvers = {
 
       throw new AuthenticationError('Not logged in');
     },
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('thoughts')
+        .populate('friends');
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
   },
 
   Mutation: {
@@ -46,7 +44,6 @@ const resolvers = {
 
       return { token, user };
     },
-
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
@@ -63,7 +60,6 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-
     addThought: async (parent, args, context) => {
       if (context.user) {
         const thought = await Thought.create({
@@ -82,7 +78,6 @@ const resolvers = {
 
       throw new AuthenticationError('You need to be logged in!');
     },
-
     addReaction: async (parent, { thoughtId, reactionBody }, context) => {
       if (context.user) {
         const updatedThought = await Thought.findOneAndUpdate(
@@ -100,7 +95,6 @@ const resolvers = {
 
       throw new AuthenticationError('You need to be logged in!');
     },
-
     addFriend: async (parent, { friendId }, context) => {
       if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
